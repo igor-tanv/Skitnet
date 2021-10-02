7401edac0912f3927881d3e7e528dc07031b95ef
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-02 10:45:05 -0400

    add generic repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,39 +6,47 @@ using System.Collections.Generic;
 
 namespace API.Controllers
 {
-  [ApiController]
+    [ApiController]
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase
     {
+    private readonly IGenericRepository<Product> _productsRepo;
+    private readonly IGenericRepository<ProductType> _productTypesRepo;
+    private readonly IGenericRepository<ProductBrand> _productBrandsRepo;
 
-        private readonly IProductRepository _repo;
-        public ProductsController(IProductRepository repo)
-        {
-        _repo = repo;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
-        {
-            return Ok(await _repo.GetProductsAsync());
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-            return await _repo.GetProductByIdAsync(id);
-        }
-
-        [HttpGet("brands")]
-        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
-        {
-            return Ok(await _repo.GetProductBrandsAsync());
-        }
-
-        [HttpGet("types")]
-        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
-        {
-        return Ok(await _repo.GetProductTypesAsync());
-        }
+    public ProductsController(
+        IGenericRepository<Product> productsRepo,
+        IGenericRepository<ProductBrand> productBrandsRepo,
+        IGenericRepository<ProductType> productTypesRepo
+    )
+    {
+        _productsRepo = productsRepo;
+        _productBrandsRepo = productBrandsRepo;
+        _productTypesRepo = productTypesRepo;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+      return Ok(await _productsRepo.ListAllASync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+      return Ok(await _productsRepo.GetByIdAsync(id));
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+    {
+      return Ok(await _productBrandsRepo.ListAllASync());
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+    {
+    return Ok(await _productTypesRepo.ListAllASync());
+    }
+   }
 }
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -29,12 +29,10 @@ namespace Infrastructure.Data
         .Include(p => p.ProductBrand)
         .FirstOrDefaultAsync(p => p.Id == id);
     }
-
     public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
     {
       return await _context.ProductBrands.ToListAsync();
     }
-
     public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
     {
       return await _context.ProductTypes.ToListAsync();
