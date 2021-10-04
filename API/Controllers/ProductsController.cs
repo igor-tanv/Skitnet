7fd75750ee4b9625f6cba5680334ab8cee5d263c
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-04 10:20:11 -0400

    add data shape with DTO

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,8 @@ using Core.Entities;
 using Core.Interfaces;
 using System.Collections.Generic;
 using Core.Specifications;
+using API.Dtos;
+using System.Linq;
 
 namespace API.Controllers
 {
@@ -27,17 +29,39 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
+    public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
     {
       var spec = new ProductsWithTypesAndBrandsSpecification();
-      return Ok(await _productsRepo.ListAllAsync(spec));
+      var products = await _productsRepo.ListAllAsync(spec);
+
+      return products.Select(product => new ProductToReturnDto
+      {
+        Id = product.Id,
+        Name = product.Name,
+        Description = product.Description,
+        PictureUrl = product.PictureUrl,
+        Price = product.Price,
+        ProductBrand = product.ProductBrand.Name,
+        ProductType = product.ProductType.Name
+      }).ToList();
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetProduct(int id)
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
-      return await _productsRepo.GetEntityWithSpec(spec);
+      var product =  await _productsRepo.GetEntityWithSpec(spec);
+
+      return new ProductToReturnDto
+      {
+        Id = product.Id,
+        Name = product.Name,
+        Description = product.Description,
+        PictureUrl = product.PictureUrl,
+        Price = product.Price,
+        ProductBrand = product.ProductBrand.Name,
+        ProductType = product.ProductType.Name
+      };
     }
 
     [HttpGet("brands")]
