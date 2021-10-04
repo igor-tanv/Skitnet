be9b2e01241cd100d2863a4e5906a0bbff91415c
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-04 11:51:47 -0400

    add custom value resolver for automapper

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -12,7 +12,8 @@ namespace API.Helpers
       CreateMap<Product, ProductToReturnDto>()
       //destination, options, source (d, o ,s)
         .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-        .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+        .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+        .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
     }
   }
 }
