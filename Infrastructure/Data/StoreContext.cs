e50aec1d2a343b6818d7e31a6e73838186a00637
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-05 08:08:32 -0400

    filter search by type and brand

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -24,15 +24,15 @@ namespace Infrastructure.Data
 
       if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
       {
-          foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-          {
-              var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+          var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
 
-              foreach (var property in properties)
-              {
-                  modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
-              }
+          foreach (var property in properties)
+          {
+              modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
           }
+        }
       }
     }
   }
