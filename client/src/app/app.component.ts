65f1f35d50fd0bed172c11b72fef6c34d81a77c1
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-12 17:37:12 -0400

    add product interface types

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   
   title = 'client';
+  products: IProduct[] = []
   
   constructor(private http: HttpClient) { }
   ngOnInit(): void {
     this.getUsers()
   }
 
-  getUsers():void {
-    this.http.get('https://localhost:5001/api/products').subscribe(res => {
-      console.log(res)
+  getUsers() {
+    this.http.get<IPagination>('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (res: IPagination) => {
+      this.products = res.data
+      console.log(res.data)
+      }, error => {
+        console.log(error)
     })
   }
 }
