87b0ba426bc0ae8974250a325b6d14a9affdff9a
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-22 14:25:13 -0400

    add basket types

--- client/src/app/basket/basket.module.ts
+++ client/src/app/basket/basket.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasketRoutingModule } from './basket-routing.module';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
