05288aa6a01c7838bc473330f300ab527796f6ef
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-25 20:39:44 -0400

    add order aggregate

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
-        console.log('initialised basket');
+        console.log('initialized basket');
       }, error => {
         console.log(error);
       });
