019fa00e603e5fcd9b3aff93940a5d70789da949
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-27 14:08:52 -0400

    add unit of work repo

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,7 @@ namespace API
 
       services.AddScoped<ITokenService, TokenService>();
       services.AddScoped<IOrderService, OrderService>();
+      services.AddScoped<IUnitOfWork, UnitOfWork>();
       services.AddScoped<IProductRepository, ProductRepository>();
       services.AddScoped<IBasketRepository, BasketRepository>();
       services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
