4927fdaac91c86fd378c13343a7959846f25475f
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-27 17:48:20 -0400

    shape orderItem return data

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -2,22 +2,30 @@ using API.Dtos;
 using AutoMapper;
 using Core.Entities;
 using Core.Entities.Identity;
+using Core.Entities.OrderAggregate;
 
 namespace API.Helpers
 {
-  public class MappingProfiles : Profile
-  {
-    public MappingProfiles()
+    public class MappingProfiles : Profile
     {
-      CreateMap<Product, ProductToReturnDto>()
-      //destination, options, source (d, o ,s)
-        .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-        .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
-        .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
-      CreateMap<Address, AddressDto>().ReverseMap();
-      CreateMap<BasketItem, BasketItemDto>().ReverseMap();
-      CreateMap<CustomerBasket, CustomerBasketDto>().ReverseMap();
-      CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+            CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
+            CreateMap<CustomerBasketDto, CustomerBasket>();
+            CreateMap<BasketItemDto, BasketItem>();
+            CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price));
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s =>  s.ItemOrdered.ProductItemId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s =>  s.ItemOrdered.ProductName))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom(s =>  s.ItemOrdered.PictureUrl))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<OrderItemUrlResolver>());
+        }
     }
-  }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,9 +1,11 @@
 
+using System;
 using System.Linq;
 using System.Reflection;
 using Core.Entities;
 using Core.Entities.OrderAggregate;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data
 {
@@ -12,7 +14,6 @@ namespace Infrastructure.Data
     public StoreContext( DbContextOptions<StoreContext> options) : base(options)
     {
     }
-
     public DbSet<Product> Products {get; set;}
     public DbSet<ProductBrand> ProductBrands {get; set;}
     public DbSet<ProductType> ProductTypes {get; set;}
@@ -30,11 +31,21 @@ namespace Infrastructure.Data
       {
         foreach (var entityType in modelBuilder.Model.GetEntityTypes())
         {
-          var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+          var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType 
+          == typeof(decimal));
+          var dateTimeProperties = entityType.ClrType.GetProperties()
+            .Where(p => p.PropertyType == typeof(DateTimeOffset));
 
           foreach (var property in properties)
           {
-              modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+              modelBuilder.Entity(entityType.Name)
+                .Property(property.Name).HasConversion<double>();
+          }
+
+          foreach (var property in dateTimeProperties)    
+          {
+            modelBuilder.Entity(entityType.Name)
+              .Property(property.Name).HasConversion(new DateTimeOffsetToBinaryConverter());
           }
         }
       }
