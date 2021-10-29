f29be328ae7083e18e756911a193d75c9eaa64c2
Igor Tatarinov <igor.tanv@gmail.com>
2021-10-28 22:16:20 -0400

    persist user login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -18,8 +18,12 @@ export class AppComponent implements OnInit {
   }
 
   loadCurrentUser() {
-    const token = localStorage.getItem('token') || ''
-    this.accountService.loadCurrentUser(token)
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('loaded user');
+    }, error => {
+      console.log(error);
+    });
   }
 
   loadBasket() {
