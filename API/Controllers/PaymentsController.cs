a618fc0ec680c05ff041483213c7e0a6a193ec7c
Igor Tatarinov <igor.tanv@gmail.com>
2021-11-02 13:01:30 -0400

    add stripe webhooks

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -1,31 +1,63 @@
+using System.IO;
 using System.Threading.Tasks;
 using API.Errors;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stripe;
+using Order = Core.Entities.OrderAggregate.Order;
 
 namespace API.Controllers
 {
   public class PaymentsController : BaseApiController
   {
     private readonly IPaymentService _paymentService;
-    public PaymentsController(IPaymentService paymentService)
+    private readonly ILogger<PaymentsController> _logger;
+
+    private string WhSecret = "";
+    public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger) 
     {
+      _logger = logger;
       _paymentService = paymentService;
     }
 
     [Authorize]
     [HttpPost("{basketId}")]
     public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+    {
+      var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+
+      if (basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
+
+      return basket;
+    }
+
+    [HttpPost("webhook")]
+    public async Task<ActionResult> StripeWebhook()
+    {
+      var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+      var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
+
+      PaymentIntent intent;
+      Order order;
+
+      switch (stripeEvent.Type)
         {
-            var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
-
-            if (basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
-
-            return basket;
+          case "payment_intent.succeeded":
+              intent = (PaymentIntent)stripeEvent.Data.Object;
+              _logger.LogInformation("Payment Succeeded: ", intent.Id);
+              //TODO: update order with new status
+              break;
+          case "payment_intent.payment_failed":
+              intent = (PaymentIntent)stripeEvent.Data.Object;
+              _logger.LogInformation("Payment Failed: ", intent.Id);
+              //TODO: update order status
+              break;
         }
-
-
+            return new EmptyResult();
+    }
   }
 }
--- Infrastructure/Services/PaymentService.cs
+++ Infrastructure/Services/PaymentService.cs
@@ -83,5 +83,7 @@ namespace Infrastructure.Services
 
             return basket;
         }
+
+    
   }
 }
